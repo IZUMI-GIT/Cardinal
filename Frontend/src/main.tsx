4e39eb9215e3978b7b8330cff56d2e9685dafcb5
Izumi <izumi.aang@gmail.com>
2025-08-27 04:38:22 +0530

    tailwind fixed

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.tsx'
+import './index.css'
 import { QueryClientProvider } from '@tanstack/react-query'
 import queryClient from './config/queryClient.ts'
 
