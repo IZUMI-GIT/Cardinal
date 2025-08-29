c1aad64ed23b0d23ebf8240af4ecd493382ebeac
IZUMI-GIT <133763814+IZUMI-GIT@users.noreply.github.com>
2025-08-29 06:30:39 +0530

    no tanstack

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,16 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import App from './App.tsx'
 import './index.css'
-import { QueryClientProvider } from '@tanstack/react-query'
-import queryClient from './config/queryClient.ts'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools position="bottom" initialIsOpen={false} />
-    </QueryClientProvider>
   </StrictMode>,
 )
