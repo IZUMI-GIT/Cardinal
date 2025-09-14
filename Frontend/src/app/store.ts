d9412da60256bfa57898ad1582313d809647df47
IZUMI-GIT <133763814+IZUMI-GIT@users.noreply.github.com>
2025-09-15 00:43:53 +0530

    AuthSlice -done

--- Frontend/src/app/store.ts
+++ Frontend/src/app/store.ts
@@ -1,5 +1,5 @@
 import {configureStore} from '@reduxjs/toolkit'
-import authReducer from '../auth/authSlice'
+import authReducer from '../components/auth/authSlice'
 
 export const store = configureStore({
 
