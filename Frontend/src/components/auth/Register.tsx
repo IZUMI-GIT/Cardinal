615c6a85c935ba866a978c12bf595539e2dd7cc4
Izumi <izumi.aang@gmail.com>
2025-09-22 19:40:35 +0530

    started register

--- Frontend/src/components/auth/Register.tsx
+++ Frontend/src/components/auth/Register.tsx
@@ -1,9 +1,37 @@
 
+
 const Register = () => {
+
     
-    return (
-        <h1>Register page</h1>
+    return(
+        <div>
+            <form>
+                <label htmlFor="email">
+                    Name: 
+                </label>
+                <input 
+                    type="text" name="name" id="name"/>
+                <label htmlFor="email">
+                    Email: 
+                </label>
+                <input 
+                    type="email" name="email" id="email"/>
+                <label>
+                    Password: 
+                </label>
+                <input 
+                    type="password" name="password" id="password"/>
+                <p>
+                    Atleast 8 characters, include a number & symbol
+                </p>
+                <label>
+                    Confirm Password:
+                </label>
+                <input type="password" name="confirm" id="confirm" />
+            </form>
+        </div>
     )
 }
+
 
 export default Register;
