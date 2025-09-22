203517193f8c64eb832f52c5e9b3ad31056e43d7
IZUMI-GIT <133763814+IZUMI-GIT@users.noreply.github.com>
2025-09-22 10:47:21 +0530

    22-09-2025

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,22 +1,101 @@
-import { lazy, Suspense, useState } from 'react'
+import { Suspense } from 'react'
 import './App.css'
 import Spinner from './features/Spinner'
-
-const Routing = lazy(() => import('./routes/Routing'))
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useEffect } from "react"
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query"
+import { useMeQuery, useRefreshMutation } from "./api/apiSlice"
+import { clearAuth, setAuthStatus, setAuthUser } from "./components/auth/authSlice"
+import { WrapperRoute } from "./routes/WrapperRoute"
+import { Board } from "./components/boards/Board"
+import { Home } from "./components/Home"
+import { Register } from "./components/auth/Register"
+import { LogInModal } from "./components/auth/LogInModal"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    data: user,
+    isSuccess,
+    isError,
+    error
+  } = useMeQuery();
 
-  return (
-    <div>
-      <div>
-        <button className='border bg-sky-500 rounded-lg px-2 hover:bg-sky-700' type="button" onClick={() => setCount(c => c+1)}>Click Me! : {count}</button>
-      </div>
-      <Suspense fallback={<Spinner />}>
-        <Routing />
-      </Suspense>
-    </div>
-  )
+  if(isSuccess){
+
+    setAuthUser({
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      role: user.role
+    });
+
+    setAuthStatus("authenticated");
+
+    return (
+      <BrowserRouter>
+      <Routes>
+        <Route element={<WrapperRoute />} >
+          <Route path="/boards" element={<Board />} />
+        </Route>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+    )
+  }else if(isError && (error as FetchBaseQueryError).status === 401){
+    
+    const refresh = useRefreshMutation;
+    useEffect(() => {refresh()}, []);
+
+    const {
+      data: user,
+      isSuccess
+    } = useMeQuery();
+
+    if(isSuccess){
+
+      setAuthUser({
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        role: user.role
+      });
+
+      setAuthStatus("authenticated");
+
+      return (
+        <BrowserRouter>
+        <Routes>
+          <Route element={<WrapperRoute />} >
+            <Route path="/boards" element={<Board />} />
+          </Route>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        </BrowserRouter>
+      )
+    }else{
+      clearAuth();
+      setAuthStatus("unauthenticated")
+
+      return(
+        <Suspense fallback={<Spinner />}>
+          <BrowserRouter>
+          <Routes>
+            <Route element={<WrapperRoute />} >
+              <Route path="/boards" element={<Board />} />
+            </Route>
+            <Route path="/" element={<Home />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<LogInModal />} />
+          </Routes>
+          </BrowserRouter>
+        </Suspense>
+      )
+    }
+  }
+
 }
 
+
 export default App
+
+
--- Frontend/src/routes/Routing.tsx
+++ Frontend/src/routes/Routing.tsx
@@ -4,29 +4,82 @@ import { LogInModal } from "../components/auth/LogInModal"
 import { Home } from "../components/Home"
 import { WrapperRoute } from "./WrapperRoute"
 import { Board } from "../components/boards/Board"
-import { useRefreshMutation } from "../api/apiSlice"
+import { useMeQuery, useRefreshMutation } from "../api/apiSlice"
+// import Spinner from "../features/Spinner"
+import { clearAuth, setAuthStatus, setAuthUser } from "../components/auth/authSlice"
 import { useEffect } from "react"
-
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query"
 
 const Routing = () => {
   
-  const [refresh] = useRefreshMutation();
-  useEffect(() => {refresh()}, []);
+  const {
+    data: user,
+    isSuccess,
+    isError,
+    error
+  } = useMeQuery();
 
-    return(
-        <>
-        <BrowserRouter>
-        <Routes>
-          <Route element={<WrapperRoute />} >
-            <Route path="/boards" element={<Board />} />
-          </Route>
-          <Route path="/" element={<Home />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/login' element={<LogInModal />} />
-        </Routes>
-      </BrowserRouter>
-        </>
-    )
+  if(isSuccess){
+
+    setAuthUser({
+      id: user.id,
+      email: user.email,
+      username: user.username,
+      role: user.role
+    });
+
+    setAuthStatus("authenticated");
+
+    // return (
+    //   <BrowserRouter>
+    //   <Routes>
+    //     <Route element={<WrapperRoute />} >
+    //       <Route path="/boards" element={<Board />} />
+    //     </Route>
+    //     <Route path="/" element={<Home />} />
+    //   </Routes>
+    // </BrowserRouter>
+    // )
+  }else if(isError && (error as FetchBaseQueryError).status === 401){
+    
+    const refresh = useRefreshMutation;
+    useEffect(() => {refresh()}, []);
+
+    const {
+      data: user,
+      isSuccess
+    } = useMeQuery();
+
+    if(isSuccess){
+
+      setAuthUser({
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        role: user.role
+      });
+
+      setAuthStatus("authenticated");
+    }else{
+      clearAuth();
+      setAuthStatus("unauthenticated")
+    }
+  }
+
+  return(
+      <>
+      <BrowserRouter>
+      <Routes>
+        <Route element={<WrapperRoute />} >
+          <Route path="/boards" element={<Board />} />
+        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<LogInModal />} />
+      </Routes>
+    </BrowserRouter>
+      </>
+  )
 }
 
 export default Routing;
