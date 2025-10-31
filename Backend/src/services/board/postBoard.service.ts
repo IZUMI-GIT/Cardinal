99ea869fd52e0c344c20722ced8f2f3b56ab9110
Izumi <izumi.aang@gmail.com>
2025-10-31 11:17:10 +0530

    signup --refactor

--- Backend/src/services/board/postBoard.service.ts
+++ Backend/src/services/board/postBoard.service.ts
@@ -1,9 +1,9 @@
-import { PrismaClient } from "@prisma/client"
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client"
+// const prisma = new PrismaClient();
 
-export const postBoard = async ( title: string, userId: number) => {
+// export const postBoard = async ( title: string, userId: number) => {
 
-    try{
-        const response = await prisma.board
-    }
-}
+//     try{
+//         const response = await prisma.board
+//     }
+// }
