4f77644c39ff901021012597da318e043d3fb1d8
Izumi <izumi.aang@gmail.com>
2025-11-04 13:01:53 +0530

    refactoring SignIn

--- Backend/src/controllers/auth/signUp.controller.ts
+++ Backend/src/controllers/auth/signUp.controller.ts
@@ -14,12 +14,12 @@ const signupSchema = z.object({
 export const postSignup = async (req: Request, res: Response, next: NextFunction) => {
 
     try{
-        const parsed = signupSchema.safeParse(req.body);
-        if(!parsed.success){
-            return next(new AppError(`Validation Error: ${z.treeifyError(parsed.error)}`, 400,))
+        const signupParsed = signupSchema.safeParse(req.body);
+        if(!signupParsed.success){
+            return next(new AppError(`Validation Error: ${z.treeifyError(signupParsed.error)}`, 400,))
         }
         
-        const {name, email, password, username} = parsed.data;
+        const {name, email, password, username} = signupParsed.data;
         const normalizedEmail = String(email).trim().toLowerCase();
         
         const userAgent = req.headers['user-agent'] ?? "unknown";
